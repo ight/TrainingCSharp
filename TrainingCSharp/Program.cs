c0dc36f4da41be0c7bd937174a7bad3919f73341
ight <thelight.prakash247@gmail.com>
2020-06-19 22:38:53 +0530

    C#: Practice

---
+++ TrainingCSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TrainingCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car;
+            car = new Car("Red");
+            Console.WriteLine(car.Describe());
+
+            car = new Car("Green");
+            Console.WriteLine(car.Describe());
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        private string color;
+
+        public Car(string color)
+        {
+            this.color = color;
+        }
+
+        public string Describe()
+        {
+            return "This car is " + Color;
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+    }
+}
